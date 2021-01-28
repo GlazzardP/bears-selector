6f6f45a17df2315d374456d988901efb3276f4e2
Paul Glazzard <paulglazzard@Pauls-MacBook-Pro.local>
2021-01-28 18:00:49 +0000

    modal working

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
+
+
+import InstructionModal from "../../containers/InstructionModal";
 
 import Button from "../Button";
 import CancelSvg from "../../assets/images/Icons/cancel.svg"
@@ -12,6 +15,7 @@ interface NavbarProps {
 // const CardFront: React.FC<CardFrontProps> = ({
 
 const Navbar: React.FC<NavbarProps> = ({signIn}) => {
+  const [instrutionModal, toggleInstructionModal] =useState<boolean>(false);
   return (
     <section className={styles.Navbar}>
       <h1>Bristol Bears Team Selector</h1>
@@ -23,7 +27,12 @@ const Navbar: React.FC<NavbarProps> = ({signIn}) => {
       <Button 
       // btnImg={CancelSvg}
        btnText="Instructions" 
-       handleClick={() => alert("Here are you instructions: 1. Log in. This means you can submit your team. 2. Pick your best Bears. 3. Submit 4. Check out who's been picked the most & share your tam with your mates.")} />
+       handleClick={() => {toggleInstructionModal(true)}} />
+      {/* //  handleClick={() => alert("Here are you instructions: 1. Log in. This means you can submit your team. 2. Pick your best Bears. 3. Submit 4. Check out who's been picked the most & share your tam with your mates.")} /> */}
+
+       {instrutionModal ? ( 
+          <InstructionModal toggleInstructionModal={toggleInstructionModal} instructionModal={instrutionModal} />
+       ) : null}
     </section>
   );
 };
--- src/containers/InstructionModal/InstructionModal.tsx
+++ src/containers/InstructionModal/InstructionModal.tsx
@@ -2,62 +2,30 @@ import React, { useEffect, useState} from "react";
 import styles from "./InstructionModal.module.scss";
 
 interface InstructionModalProps { 
-  removeOverlay:() => any;
+  toggleInstructionModal:(input: boolean) => any;
+  instructionModa: boolean;
 }
 
-const InstructionModal: React.FC<InstructionModalProps> = () => {
+const InstructionModal: React.FC<InstructionModalProps> = ({toggleInstructionModal, instructionModa}) => {
 
   return (
-    <div className={styles.overlayBackground}>
+    <section className={styles.instructionModalContainer} onClick={() => toggleInstructionModal(false)}>
+      <div className={styles.instructionModalBox}>
+        <h2>Instructions</h2>
+        <p>We have build this website for rugby fans, especially Bristol Bear fan to pick their favourite team.</p>
+        <p>1. We recommend logging in. This means you can submit your team to our database. The more people who do this, the better we can see who the preferred players are.<br></br>To log in, you can use your Gmail account, Facebook or create your own account with your email and password. </p>
+        <p>2. Pick your 15. It's pretty simple. You can use the filter tool at the top of the players to find the right player for the right position.</p>
+        <p>3. Each player is listed with their potential playing positions. So you can choose where you want them to play.</p>
+        <p>4. Once you have picked your 15, you can submit your team. I wouldn't go calling myself Pat Lam just yet!</p>
+        <p>5. You can know see how many times each player has been picked.</p>
 
-    </div>
+        <p>Moving forward we want to be able to include more information and more insights but this is largely dependant on your support. No, not financial. Just spreading the word. Either by the good ol' fashion way, by mouth or, once you have submitted your team, you can share on all the horrible social media platforms.</p>
+
+
+        <button onClick={() => {toggleInstructionModal(false)}}>Close</button>
+      </div>
+    </section>
   );
 };
 
 export default InstructionModal;
-
-// const { useState } = React;
-
-const stopProp = (e: Event) => {
-  e.stopPropagation();
-};
-
-// const LoginOverlay = ({ removeOverlay }) => {
-
-//   return (
-//     <div className={styles.overlayBackground} onClick={(e) => removeOverlay()}>
-//       <div className={styles.overlayCard} onClick={(e) => stopProp(e)}>
-//         <form
-//           onSubmit={(e) => {
-//             e.preventDefault();
-//             removeOverlay();
-//           }}
-//         >
-//           <button className="formSubmit" 
-//           // type="sbmit"
-//           >
-//             Noverlay
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const NoAuthWebsite = () => {
-//   const [overlay, setOverlay] = useState(false);
-
-//   return (
-//     <div className={styles.flexColumn}>
-//       <div className={overlay ? "overlayShown" : "overlayHidden"}>
-//         <LoginOverlay removeOverlay={() => setOverlay(false)} />
-//       </div>
-      
-//       <div className="loginButton" onClick={() => setOverlay(true)}>
-//         Overlay
-//       </div>
-//     </div>
-//   );
-// };
-
-// ReactDOM.render(<NoAuthWebsite />, document.getElementById("root"));
